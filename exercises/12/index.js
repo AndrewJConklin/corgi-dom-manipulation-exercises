52a133450adeda47c9f8fc430692c6c74fba9951
aconkli9 <aconkli9@ford.com>
2022-03-30 17:33:22 -0400

    Remove comment

--- exercises/12/index.js
+++ exercises/12/index.js
@@ -1,5 +1,3 @@
-// Create a new `<ul>` element. Append an `<li>` with the text "Kiki" to it. Append the `<ul>` to the `.queens` `<div>`
-
 const ul = document.createElement("ul")
 const li = document.createElement("li")
 
