5babb2c1ef5ff1a773556869485479db75b24e73
aconkli9 <aconkli9@ford.com>
2022-03-30 13:22:17 -0400

    Add solution 3

---
+++ exercises/3/index.js
@@ -0,0 +1,3 @@
+const img = document.querySelector("img")
+
+img.src = "https://www.thesprucepets.com/thmb/rD_MTRs7g-qkad_CxO3wgY1W_Xg=/941x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/40926432_560505141051912_3896594467564281999_n-5ba052bac9e77c0050e669bb.jpg"
